6f18b415b2e30f6d5d093c0eed76414f84e7fd60
David Ortinau <david.ortinau@microsoft.com>
2017-10-16 07:27:06 -0500

    basic app to show project structure

---
+++ StandardizeMe.Shared/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace StandardizeMe
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
